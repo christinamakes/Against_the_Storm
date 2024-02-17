e2a2df439eda20875061ba4da4bf8ee57c908fd1
Christina Moore <sayhitochristina@gmail.com>
2024-02-17 13:25:29 -0600

    config supabase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,33 @@
 import express, { Request, Response } from "express";
+import { createClient } from "@supabase/supabase-js"
+import { Database } from './supabase'
+import 'dotenv/config'
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello");
-})
+const supabaseUrl = 'https://wnbtubmfobgomfgfjikb.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY as string;
+const supabase = createClient<Database>(
+    supabaseUrl, supabaseKey);
 
-app.listen(5000);
+app.get('/', async (req: Request, res: Response) => {
+    try {
+        const { data, error } = await supabase
+            .from('resources')
+            .select('*');
+
+        if (error) {
+            throw error;
+        }
+
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// beam me up, scotty
+app.listen(PORT, () => {
+    console.log(`hi from http://localhost:${PORT}`);
+});
